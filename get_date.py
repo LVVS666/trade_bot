4bb6bf0446fcce9717a9a52afaffc73b9aafae95
Василий Логинов <97108615+LVVS666@users.noreply.github.com>
2023-11-18 18:45:34 -0800

    okx_get_balance

--- get_date.py
+++ get_date.py
@@ -1,8 +1,10 @@
 import os
 import requests
 
+from coinbase.wallet.client import Client
 from okx.MarketData import MarketAPI
 from dotenv import load_dotenv
+
 import data
 
 load_dotenv()
@@ -16,7 +18,7 @@ def get_price_coin_in_mexc(coin):
     url_api_mexc = "https://api.mexc.com/api/v3/ticker/price"
     params = {
         'symbol': f'{coin.name}{coin.currency}',
-        'interval': '5m',
+        'interval': '1m',
         'limit': 10
     }
     price_coin_mexc = requests.get(url_api_mexc, params=params).json()['price']
@@ -29,8 +31,14 @@ def get_price_coin_in_okx(coin):
     price_coin_okx = api.get_ticker(f'{coin.name}-{coin.currency}')['data'][0]['last']
     return price_coin_okx
 
+def get_proce_coin_in_coinbase(coin):
+    client = Client(api_key='КЛЮЧ АПИ', api_secret='СЕКРЕТНЫЙ КЛЮЧ')
+    price_coin_coin_base = client.get_spot_price(currency_pair=f'{coin.name}-{coin.currency}')
+    return price_coin_coin_base
+
 
 def get_comparison_price(coin, network, market):
     '''Сравнение цен на разных биржах'''
     pass
+
 

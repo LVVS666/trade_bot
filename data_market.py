e953a952c1b42bcaa94be74c18b54d664bc0166c
Василий Логинов <97108615+LVVS666@users.noreply.github.com>
2023-11-19 18:00:29 -0800

    create_class_MEXC

--- data_market.py
+++ data_market.py
@@ -1,4 +1,5 @@
 import os
+import requests
 
 from models import Market
 from okx.MarketData import MarketAPI
@@ -8,7 +9,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
-class OKX_Models(Market):
+class OKXModel(Market):
     '''Модель класса OKX'''
     def get_price_coin_in_okx(self, coin):
         '''Получение цены монеты на okx '''
@@ -17,10 +18,31 @@ class OKX_Models(Market):
         return price_coin_okx
 
 
-OKX = OKX_Models(
+class MEXCModel(Market):
+    '''Модель класса MEXC'''
+    def get_price_coin_in_mexc(self, coin):
+        '''Получение цены монеты на mexc'''
+        url_api_mexc = "https://api.mexc.com/api/v3/ticker/price"
+        params = {
+            'symbol': f'{coin.name}{coin.currency}',
+            'interval': '1m',
+            'limit': 10
+        }
+        price_coin_mexc = requests.get(url_api_mexc, params=params).json()['price']
+        return price_coin_mexc
+
+
+OKX = OKXModel(
                 'OKX',
                 2,
                 api_key=os.getenv('API_KEY_OKX'),
                 secret_key=os.getenv('SECRET_KEY_OKX'),
                 passphrase=os.getenv('PASSPHRASE_OKX')
+                )
+
+MEXC = MEXCModel(
+                'MEXC',
+                2,
+                api_key=os.getenv('API_KEY_MEXC'),
+                secret_key=os.getenv('SECRET_KEY_MEXC')
                 )
--- get_date.py
+++ get_date.py
@@ -1,28 +1,6 @@
-import os
 import requests
 
 from coinbase.wallet.client import Client
-from dotenv import load_dotenv
-
-import data_coin
-
-load_dotenv()
-
-API_KEY_MEXC = os.getenv('API_KEY_MEXC')
-SECRET_KEY_MEXC = os.getenv('SECRET_KEY_MEXC')
-
-
-def get_price_coin_in_mexc(coin):
-    '''Получение цены монеты на mexc'''
-    url_api_mexc = "https://api.mexc.com/api/v3/ticker/price"
-    params = {
-        'symbol': f'{coin.name}{coin.currency}',
-        'interval': '1m',
-        'limit': 10
-    }
-    price_coin_mexc = requests.get(url_api_mexc, params=params).json()['price']
-    return price_coin_mexc
-
 
 def get_price_coin_in_coinsbit(coin):
     '''Получение цены монеты на coinsbit'''

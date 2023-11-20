ed1ce5782a7a231142f1fcbc721e7956734a9cf0
Василий Логинов <97108615+LVVS666@users.noreply.github.com>
2023-11-19 19:11:19 -0800

    create_class_bitget

--- get_date.py
+++ get_date.py
@@ -2,6 +2,9 @@ import requests
 
 from coinbase.wallet.client import Client
 
+import data_coin
+
+
 def get_price_coin_in_coinsbit(coin):
     '''Получение цены монеты на coinsbit'''
     url_api_coinsbit = requests.get(' https://coinsbit.io/api/v1/public/tickers')
@@ -20,3 +23,10 @@ def get_price_coin_in_kucoin(coin):
     '''Получение цены монеты на Kucoin'''
     response = requests.get(f'https://api.kucoin.com/api/v1/market/orderbook/level1?symbol={coin.name}-{coin.currency}')
     return response.json()['data']['price']
+
+
+def get_price_coin_in_bitget(coin):
+    '''Получение цены монеты на Bitget'''
+
+    response = requests.get(f'https://api.bitget.com/api/v2/spot/market/tickers?symbol={coin.name}{coin.currency}')
+    return response.json()['data'][0]['lastPr']

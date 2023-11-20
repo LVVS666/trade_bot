a5b152cadfd4e10e64aa6094a927743ef20de068
Василий Логинов <97108615+LVVS666@users.noreply.github.com>
2023-11-19 17:18:35 -0800

    get_price_kucoin

--- get_date.py
+++ get_date.py
@@ -24,23 +24,22 @@ def get_price_coin_in_mexc(coin):
     return price_coin_mexc
 
 
-def get_price_coinsbit(coin):
+def get_price_coin_in_coinsbit(coin):
     '''Получение цены монеты на coinsbit'''
     url_api_coinsbit = requests.get(' https://coinsbit.io/api/v1/public/tickers')
     price_coin_coinsbit = url_api_coinsbit.json()['result'][f'{coin.name}_{coin.currency}']['ticker']['last']
     return price_coin_coinsbit
 
 
-def get_proce_coin_in_coinbase(coin):
+def get_price_coin_in_coinbase(coin):
     '''Получение цены монеты на coinbase'''
     client = Client(api_key='КЛЮЧ АПИ', api_secret='СЕКРЕТНЫЙ КЛЮЧ')
     price_coin_coin_base = client.get_spot_price(currency_pair=f'{coin.name}-{coin.currency}')
     return price_coin_coin_base
 
 
-def get_comparison_price(coin, network, market):
-    '''Сравнение цен на разных биржах'''
-    pass
-
-
+def get_price_coin_in_kucoin(coin):
+    '''Получение цены монеты на Kucoin'''
+    response = requests.get(f'https://api.kucoin.com/api/v1/market/orderbook/level1?symbol={coin.name}-{coin.currency}')
+    return response.json()['data']['price']
 

21cf3344ed3702d8eb684fe310746bae21d8fa7d
Василий Логинов <97108615+LVVS666@users.noreply.github.com>
2023-11-19 19:15:18 -0800

    create_class_bitget

--- get_date.py
+++ get_date.py
@@ -27,6 +27,5 @@ def get_price_coin_in_kucoin(coin):
 
 def get_price_coin_in_bitget(coin):
     '''Получение цены монеты на Bitget'''
-
     response = requests.get(f'https://api.bitget.com/api/v2/spot/market/tickers?symbol={coin.name}{coin.currency}')
     return response.json()['data'][0]['lastPr']

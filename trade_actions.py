7aa0604dead77053414b78775a532330debd9ccc
Василий Логинов <97108615+LVVS666@users.noreply.github.com>
2023-12-03 15:53:11 +0700

    create_func_search_spread

--- trade_actions.py
+++ trade_actions.py
@@ -11,8 +11,8 @@ def get_spread(coin):
     }
     max_price = max(last_prices_market.items(), key=lambda x: x[1])
     min_price = min(last_prices_market.items(), key=lambda x: x[1])
-    coins_one = (100 / max_price[1]) * min_price[1]
-    coins_two = ((100-0.2) / min_price[1]) * max_price[1]
+    coins_one = (50 / max_price[1]) * min_price[1]
+    coins_two = (50 / min_price[1]) * max_price[1]
     spread = max(coins_one, coins_two) - min(coins_one, coins_two)
     spread_date = {'spread': spread, 'max_price': max_price, 'min_price': min_price}
     return spread_date
@@ -21,11 +21,10 @@ async def search_spread_to_market(bot, CHAT_ID):
     while True:
         for coin in data_coin.coins:
             spread_date = get_spread(coin)
-            while spread_date['spread'] > 1:
+            if spread_date['spread'] > 1.5:
                 text_message = f'Покупка {coin.name} на бирже ' \
                                f'{spread_date["min_price"][0]} ' \
                                f'и продажа на бирже {spread_date["max_price"][0]},' \
                                f'принесет {spread_date["spread"]} USDT'
                 await bot.send_message(chat_id=CHAT_ID, text=text_message)
-                sleep(5)
-                spread_date = get_spread(coin)
+
